18585f4a0a5d378af5ed7337825f6cf05065f132
rubenciranni <rubenciranni6@gmail.com>
2023-12-10 22:50:44 +0100

    Backend: minor fix

--- service/api/liveness.go
+++ service/api/liveness.go
@@ -11,6 +11,5 @@ import (
 func (rt *_router) liveness(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	if err := rt.db.Ping(); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return
 	}
 }
