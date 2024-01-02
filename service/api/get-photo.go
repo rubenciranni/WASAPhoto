ae06c66078c789582256f618a17d42167d4197d8
rubenciranni <rubenciranni6@gmail.com>
2024-01-02 12:39:28 +0100

    Backend: Removed useless code from get-photo

--- service/api/get-photo.go
+++ service/api/get-photo.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"database/sql"
-	"errors"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -20,31 +18,6 @@ func (rt *_router) getPhoto(w http.ResponseWriter, r *http.Request, ps httproute
 	if !req.IsValid() {
 		ctx.Logger.Error("error validating request")
 		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Retrieve author of the photo
-	ctx.Logger.Debugf("retrieving photo authorId from database")
-	authorId, err := rt.db.GetPhotoAuthorId(photoId)
-	if errors.Is(err, sql.ErrNoRows) {
-		ctx.Logger.WithError(err).Error("error retrieving photo authorId from database")
-		w.WriteHeader(http.StatusNotFound)
-		return
-	} else if err != nil {
-		ctx.Logger.WithError(err).Error("error retrieving photo authorId from database")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Check if logged-in user is banned by author of the photo
-	ctx.Logger.Debugf(`checking if ban (bannerId: "%s", bannedId "%s") exists in database`, authorId, ctx.User.UserId)
-	if banned, err := rt.db.ExistsBan(authorId, ctx.User.UserId); err != nil {
-		ctx.Logger.WithError(err).Error("error searching ban in database")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	} else if banned {
-		ctx.Logger.Error("requested user is banned by logged-in user")
-		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
